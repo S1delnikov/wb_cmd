004bb687ed63ff80775e560687cb15dd309d1d1a
Misha Sidelnikov <misha.s1d@mail.ru>
2026-09-21 23:40:07 +0300

    edit root script logic

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import argparse
 from service.autochrome import parse_product_cards
+
 parser = argparse.ArgumentParser(description='Данный скрипт автоматически открывает браузер Google Chrome \
                                  и считывает информацию по карточкам товаров на Wildberries. Полученные \
                                  данные сохраняются в файл.')
 parser.add_argument('url_cards', type=str, help='Ссылка на результат поиска товаров')
 parser.add_argument('outfile', type=str, help='Файл, в который нужно сохранить результат')
 args = parser.parse_args()
-print(args.url_cards)
-print(args.outfile)
+
+print("=============================")
+print("========Начало запроса=======")
+print("=============================")
 
 parse_product_cards(args.url_cards, args.outfile)
+
+print('\n')
+print("=============================")
+print("===Запрос выполнен успешно===")
+print("=============================")
